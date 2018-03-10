1238da4af97487b72f7d3c57ad368acaa0a9ace4
admin <admin@192.168.0.200>
2018-03-10 16:36:27 +0800

    jax ws rpc

--- jax-ws-rpc-server/src/com/javatpoint/HelloWorldPublisher.java
+++ jax-ws-rpc-server/src/com/javatpoint/HelloWorldPublisher.java
@@ -1,10 +1,12 @@
 package com.javatpoint;
+
 import javax.xml.ws.Endpoint;
+
 //Endpoint publisher
-public class HelloWorldPublisher{
- 
+public class HelloWorldPublisher {
+
 	public static void main(String[] args) {
-	   Endpoint.publish("http://localhost:7779/ws/hello", new HelloWorldImpl());
-    }
- 
+		Endpoint.publish("http://localhost:7779/ws/hello", new HelloWorldImpl());
+	}
+
 }
