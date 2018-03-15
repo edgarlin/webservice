1699eff8d1b04d193ea9963ed4c30f724b1813ec
admin <admin@192.168.0.200>
2018-03-15 21:32:40 +0800

    reorg soap server/client

---
+++ soapClient/src/main/java/hello/App.java
@@ -0,0 +1,26 @@
+
+package hello;
+
+import hello.wsdl.GetCountryResponse;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+	@Bean
+	CommandLineRunner xxx(CountryClient bean001) {
+		return args -> {
+			GetCountryResponse response = bean001.getCountry("Spain");
+			System.err.println(response.getCountry().getCapital());
+		};
+	}
+
+}
