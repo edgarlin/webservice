98bd9c671b04b1109fcc51a97ccec067bbaad02a
edgar <edgar>
2018-07-05 21:02:21 +0800

    update

--- uploadsample/src/com/sample/Update.java
+++ uploadsample/src/com/sample/Update.java
@@ -1,6 +1,9 @@
 package com.sample;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,8 +15,8 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/update")
 public class Update extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -21,20 +24,38 @@ public class Update extends HttpServlet {
         super();
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println(request.getParameter("i"));
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter pwriter = response.getWriter();
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("doPost");
-		doGet(request, response);
-	}
+        response.setContentType("text/plain; charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        // response.setHeader("Content-Disposition", "attachment; filename=\"output.txt\"");
+        pwriter.println("test\n");
+
+        Enumeration<String> headerNames = request.getHeaderNames();
+
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            pwriter.append(headerName).append(":");
+            String headerValue = request.getHeader(headerName);
+            pwriter.append(headerValue).append("\n");
+        }
+
+        /*
+         * while (request.getHeaderNames().hasMoreElements()) { pwriter.append(request.getHeaderNames().nextElement()); }
+         */
+        pwriter.append("中文: ").append(request.getContextPath());
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("doPost");
+        doGet(request, response);
+    }
 
 }
